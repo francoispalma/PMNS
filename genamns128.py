bc6640852baa30d629896310d23e373f53c38cdc
Francois <francois-palma@etud.univ-tln.fr>
2022-04-07 11:18:35 +0200

    progress

--- genamns128.py
+++ genamns128.py
@@ -3,6 +3,7 @@ from math import ceil
 
 from precalcs128 import do_precalcs as do_precalcs128
 from precalcs import do_precalcs
+from primes1024 import PRIMES1024
 
 #generate P
 #K = GF(P)
@@ -12,16 +13,60 @@ from precalcs import do_precalcs
 #lone factor => nth root of lambda.
 
 if __name__ == "__main__":
-	p = 2**1024
-	print("[", end="")
-	for i in range(999):
-		p = previous_prime(p)
-		print(p, end=",\n")
-	print(previous_prime(p), end="")
-	print("]")
-#		K = GF(p)
-#		polK = PolynomialRing(K, 'X')
-#		E = polK("X^9 - 2")
-#		fs = factor(E)
-#		print(fs[0][0].degree())
-
+#	p = 2**1024
+#	print("[", end="")
+#	for i in range(999):
+#		p = previous_prime(p)
+#		print(p, end=",\n")
+#	print(previous_prime(p), end="")
+#	print("]")
+	print("pmnsdict = {}")
+	print("pmns128dict = {}")
+	for i in range(10):
+		p = PRIMES1024[i]
+		K = GF(p)
+		polK = PolynomialRing(K, 'X')
+		n = 9
+		flag = False
+		while True:
+			for lam in range(2, 8):
+				E = polK("X^" + str(n) +" - " + str(lam))
+				Eprime = polK("X^" + str(n) +" + " + str(lam))
+				fs = factor(E)
+				fsprime = factor(Eprime)
+				if fs[0][0].degree() == 1:
+					flag = True
+					lamb = lam
+					break
+				if fsprime[0][0].degree() == 1:
+					flag = True
+					fs = fsprime
+					lamb = -lam
+					break
+			if flag == True:
+				break
+			n += 1
+		#assert pow(fs[0][0][0], n, p) == (p - lamb) % p
+		print("pmns128dict[" + str(p) + "] = (" + str(p) + ", " + str(n) + ", " + str(fs[0][0][0]) + ", " + str(lamb) + ")")
+		n = 17
+		flag = False
+		while True:
+			for lam in range(2, 8):
+				E = polK("X^" + str(n) +" - " + str(lam))
+				Eprime = polK("X^" + str(n) +" + " + str(lam))
+				fs = factor(E)
+				fsprime = factor(Eprime)
+				if fs[0][0].degree() == 1:
+					flag = True
+					lamb = lam
+					break
+				if fsprime[0][0].degree() == 1:
+					flag = True
+					fs = fsprime
+					lamb = -lam
+					break
+			if flag == True:
+				break
+			n += 1
+		#assert pow(fs[0][0][0], n, p) == (p - lamb) % p
+		print("pmnsdict[" + str(p) + "] = (" + str(p) + ", " + str(n) + ", " + str(fs[0][0][0]) + ", " + str(lamb) + ")")
