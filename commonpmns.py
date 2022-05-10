8fdd740fec1c8d2705eac5aed72112b69d55fefa
Francois <francois-palma@etud.univ-tln.fr>
2022-05-10 08:27:19 +0200

    progress

--- commonpmns.py
+++ commonpmns.py
@@ -1,12 +1,12 @@
-from generated1024pmns import pmnsdict as pmnsdict1024
-from generated2048pmns import pmnsdict as pmnsdict2048
-from generated4096pmns import pmnsdict as pmnsdict4096
-from generated1024pmns128 import pmns128dict as pmns128dict1024
-from generated2048pmns128 import pmns128dict as pmns128dict2048
-from generated4096pmns128 import pmns128dict as pmns128dict4096
-from primes1024 import PRIMES1024
-from primes2048 import PRIMES2048
-from primes4096 import PRIMES4096
+from generated.generated1024pmns import pmnsdict as pmnsdict1024
+from generated.generated2048pmns import pmnsdict as pmnsdict2048
+from generated.generated4096pmns import pmnsdict as pmnsdict4096
+from generated.generated1024pmns128 import pmns128dict as pmns128dict1024
+from generated.generated2048pmns128 import pmns128dict as pmns128dict2048
+from generated.generated4096pmns128 import pmns128dict as pmns128dict4096
+from generated.primes1024 import PRIMES1024
+from generated.primes2048 import PRIMES2048
+from generated.primes4096 import PRIMES4096
 
 pmnsdicts = {1024:pmnsdict1024, 2048:pmnsdict2048, 4096:pmnsdict4096,
 	1024128: pmns128dict1024, 2048128:pmns128dict2048, 4096128: pmns128dict4096}
--- histo.py
+++ histo.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 #	print("Ratio:", mean(LL)/mean(LLL))
 #	plt.title("clocks, clocks128new, clocks128")
 #	plt.figure()
-	with open("results", "r") as f:
+	with open("results64", "r") as f:
 		L = f.readlines()
 	LL = [eval(elem[:-1]) for elem in L]
 	mini, maxi, meani = ([elem[0] for elem in LL], [elem[1] for elem in LL],
@@ -31,6 +31,18 @@ if __name__ == "__main__":
 	plt.hist(maxi)
 	plt.hist(meani)
 	plt.title("Min/Mean/Max")
+	plt.figure()
+	with open("results1024", "r") as f:
+		L = f.readlines()
+	LL = [eval(elem[:-1]) for elem in L]
+	mini, maxi, meani = ([elem[0] for elem in LL], [elem[1] for elem in LL],
+		[elem[2] for elem in LL])
+	nmin64 = mean(mini)
+	nmean64 = mean(meani)
+	plt.hist(mini)
+	plt.hist(maxi)
+	plt.hist(meani)
+	plt.title("New Min/Mean/Max")
 	plt.figure()
 	with open("results128", "r") as f:
 		L = f.readlines()
@@ -44,8 +56,9 @@ if __name__ == "__main__":
 	plt.hist(meani)
 	plt.title("Min/Mean/Max 128")
 	print("Ratio:", mean128/mean64, "(" + str(min128/min64) + ")")
+	print("New Ratio:", mean128/nmean64, "(" + str(min128/nmin64) + ")")
 	plt.figure()
-	with open("results2048", "r") as f:
+	with open("results204864", "r") as f:
 		L = f.readlines()
 	LL = [eval(elem[:-1]) for elem in L]
 	mini, maxi, meani = ([elem[0] for elem in LL], [elem[1] for elem in LL],
@@ -56,6 +69,18 @@ if __name__ == "__main__":
 	plt.hist(maxi)
 	plt.hist(meani)
 	plt.title("Min/Mean/Max 2048")
+	plt.figure()
+	with open("results2048", "r") as f:
+		L = f.readlines()
+	LL = [eval(elem[:-1]) for elem in L]
+	mini, maxi, meani = ([elem[0] for elem in LL], [elem[1] for elem in LL],
+		[elem[2] for elem in LL])
+	nmin64 = mean(mini)
+	nmean64 = mean(meani)
+	plt.hist(mini)
+	plt.hist(maxi)
+	plt.hist(meani)
+	plt.title("New Min/Mean/Max 2048")
 	plt.figure()
 	with open("results2048128", "r") as f:
 		L = f.readlines()
@@ -69,29 +94,30 @@ if __name__ == "__main__":
 	plt.hist(meani)
 	plt.title("Min/Mean/Max 2048128")
 	print("Ratio 2048:", mean128/mean64, "(" + str(min128/min64) + ")")
-	plt.figure()
-	with open("results4096", "r") as f:
-		L = f.readlines()
-	LL = [eval(elem[:-1]) for elem in L]
-	mini, maxi, meani = ([elem[0] for elem in LL], [elem[1] for elem in LL],
-		[elem[2] for elem in LL])
-	min64 = mean(mini)
-	mean64 = mean(meani)
-	plt.hist(mini)
-	plt.hist(maxi)
-	plt.hist(meani)
-	plt.title("Min/Mean/Max 4096")
-	plt.figure()
-	with open("results4096128", "r") as f:
-		L = f.readlines()
-	LL = [eval(elem[:-1]) for elem in L]
-	mini, maxi, meani = ([elem[0] for elem in LL], [elem[1] for elem in LL],
-		[elem[2] for elem in LL])
-	min128 = mean(mini)
-	mean128 = mean(meani)
-	plt.hist(mini)
-	plt.hist(maxi)
-	plt.hist(meani)
-	plt.title("Min/Mean/Max 4096128")
-	print("Ratio 4096:", mean128/mean64, "(" + str(min128/min64) + ")")
+	print("New Ratio 2048:", mean128/nmean64, "(" + str(min128/nmin64) + ")")
+#	plt.figure()
+#	with open("results4096", "r") as f:
+#		L = f.readlines()
+#	LL = [eval(elem[:-1]) for elem in L]
+#	mini, maxi, meani = ([elem[0] for elem in LL], [elem[1] for elem in LL],
+#		[elem[2] for elem in LL])
+#	min64 = mean(mini)
+#	mean64 = mean(meani)
+#	plt.hist(mini)
+#	plt.hist(maxi)
+#	plt.hist(meani)
+#	plt.title("Min/Mean/Max 4096")
+#	plt.figure()
+#	with open("results4096128", "r") as f:
+#		L = f.readlines()
+#	LL = [eval(elem[:-1]) for elem in L]
+#	mini, maxi, meani = ([elem[0] for elem in LL], [elem[1] for elem in LL],
+#		[elem[2] for elem in LL])
+#	min128 = mean(mini)
+#	mean128 = mean(meani)
+#	plt.hist(mini)
+#	plt.hist(maxi)
+#	plt.hist(meani)
+#	plt.title("Min/Mean/Max 4096128")
+#	print("Ratio 4096:", mean128/mean64, "(" + str(min128/min64) + ")")
 	plt.show()
