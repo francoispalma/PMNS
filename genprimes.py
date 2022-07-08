0e1f227d42e392602f74c48267e7aac50d5aa16b
palma <francois-palma@etd.univ-tln.fr>
2022-07-08 11:07:08 +0200

    Adds new prime size for comparison with prior code. Corresponding PMNS in generation.

--- genprimes.py
+++ genprimes.py
@@ -1,8 +1,23 @@
+import sys
+
 from sage.all import previous_prime
 
-with open("generated/primes8192.py", "w+") as f:
-	f.write("PRIMES8192 = []")
-	p = 2**8192
-	for _ in range(1000):
-		p = previous_prime(p)
-		f.write("\nPRIMES8192 += [" + str(p) + "]")
+
+if __name__ == "__main__":
+	if len(sys.argv) >= 2:
+		strg = sys.argv[1]
+		try:
+			psize = int(strg)
+			if psize < 32:
+				print("Prime Size not handled")
+				exit()
+			with open(f"generated/primes{strg}.py", "w+") as f:
+				f.write(f"PRIMES{strg} = []")
+				p = 2**psize
+				for _ in range(1000):
+					p = previous_prime(p)
+					f.write(f"\nPRIMES{strg} += [" + str(p) + "]")
+		except ValueError:
+			print("Invalid arguments")
+	else:
+		print("Not enough arguments: Psize")
