a496b507cce5f81ca95bc0854f58a9b4e15d7a74
francoispalma <francois-palma@etud.univ-tln.fr>
2022-07-14 10:24:24 +0200

    generating a new batch of prime numbers, new pmns generation is next. Some preliminary new performance results on the new computer. More to come.

--- genprimes.py
+++ genprimes.py
@@ -1,6 +1,6 @@
 import sys
 
-from sage.all import previous_prime
+from sage.all import random_prime
 
 
 if __name__ == "__main__":
@@ -13,10 +13,12 @@ if __name__ == "__main__":
 				exit()
 			with open(f"generated/primes{strg}.py", "w+") as f:
 				f.write(f"PRIMES{strg} = []")
-				p = 2**psize
-				for _ in range(1000):
-					p = previous_prime(p)
-					f.write(f"\nPRIMES{strg} += [" + str(p) + "]")
+				primeset = set()
+				while len(primeset) < 1000:
+					p = random_prime(2**psize - 1, lbound= 2**(psize - 1))
+					if p not in primeset:
+						primeset.add(p)
+						f.write(f"\nPRIMES{strg} += [" + str(p) + "]")
 		except ValueError:
 			print("Invalid arguments")
 	else:
