e0f9eb93c8876ba0ecd26ea5e16745f1074c23b4
francoispalma <francois-palma@etud.univ-tln.fr>
2022-09-13 09:04:29 +0200

    testing something

--- pyparams.py
+++ pyparams.py
@@ -1,5 +1,5 @@
 from commonpmns import pmnsdicts
 from commonpmns import primesdict
 
-(p, n, gamma, lam, rho, M, M1) = pmnsdicts[1024][primesdict[1024][1]]
+(p, n, gamma, lam, rho, M, M1) = pmnsdicts[8192][primesdict[8192][1]]
 phi = 2**64
